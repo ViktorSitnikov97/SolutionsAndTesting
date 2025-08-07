5d9b85918b8ed22e3b4c5c66f0b24fc937432b78
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-08-08 00:40:01 +0300

    add task1

--- project-root/lambdas/src/main/java/io/tasks/task1/App.java
+++ project-root/lambdas/src/main/java/io/tasks/task1/App.java
@@ -1,4 +1,44 @@
 package io.tasks.task1;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class App {
+    public static void main(String[] args) {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+        System.out.println(Arrays.deepToString(image));
+
+        String[][] enlargedImage = App.enlargeArrayImage(image);
+        System.out.println(Arrays.deepToString(enlargedImage));
+    }
+    public static String[][] enlargeArrayImage(String[][] figure) {
+        String[][] verticalEnlarge = Stream.of(figure)
+                .flatMap(array ->  Stream.of(array, array))
+                .toArray(String[][]::new);
+        return Stream.of(verticalEnlarge)
+                .map(row -> Arrays.stream(row)
+                        .flatMap(s -> Stream.of(s, s))
+//                        .map(s -> s + s)
+                        .toArray(String[]::new)
+                )
+                .toArray(String[][]::new);
+
+
+    }
+//    private static String[] getFlatArray(String[][] array) {
+//        String[] result = new String[array.length * array.length];
+//        int k = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                result[k++] = array[i][j];
+//            }
+//        }
+//        System.out.println(Arrays.toString(result));
+//        return result;
+//    }
 }
